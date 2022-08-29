b13d58d5fc6a69d6d4bcbb0ecf3ae6e272e7dd36
OmarKnows <omarknows98@gmail.com>
2022-08-29 14:02:58 +0200

    Login page added

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { Card, Form, Row, Col, Button } from 'react-bootstrap';
+import logo from '../../assets/icon.png';
 
 const LoginScreen = () => {
-  return <div>LoginScreen</div>;
+  return (
+    <div>
+      <div className="logincard">
+        <Card className="p-3 mt-5">
+          <div>
+            <img className="rounded mx-auto d-block" src={logo} alt="logo" />
+          </div>
+          <div className="my-4">
+            <h1 className="text-center mainTitle">منظومة البريد</h1>
+          </div>
+          <Form>
+            <Form.Group>
+              <Row>
+                <Col>
+                  <Form.Select>
+                    <option>departments</option>
+                  </Form.Select>
+                </Col>
+                <Col>
+                  <Form.Select>
+                    <option>usernames</option>
+                  </Form.Select>
+                </Col>
+              </Row>
+            </Form.Group>
+            <Form.Group className="form-group py-3">
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <div className="d-grid gap-2">
+              <Button type="submit" className="btn btn-primary my-3">
+                Login
+              </Button>
+            </div>
+          </Form>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default LoginScreen;
